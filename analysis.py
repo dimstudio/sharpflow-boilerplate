8063c6d4edbabf184518e3c1bb38277408d3f920
Dimstudio <daniele@dimstudio.org>
2021-05-27 15:39:14 +0200

    added the pushup datasets

--- analysis.py
+++ analysis.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     data_folder = "manual_sessions/"
     ignore_files = []
     to_exclude = ['OenName', 'RecordingID', 'ApplicationName']
-    target_classes = ['mistake']
+    target_classes = ['mistake_back']
     tensor_data, annotations, attributes = data_helper.get_data_from_files(data_folder, ignore_files=ignore_files,
                                                                            res_rate=25,
                                                                            to_exclude=to_exclude)
@@ -15,8 +15,8 @@ if __name__ == "__main__":
     X = tensor_data
     y = annotations.reset_index(drop=True)
 
-    df = annotations[['recordingID', 'mistake', 'end', 'start', 'duration']].copy()
-    df['gsr'] = np.mean(tensor_data[:, :, 1], axis=1)
+    df = annotations[['recordingID', 'mistake_back', 'end', 'start', 'duration']].copy()
+    #df['gsr'] = np.mean(tensor_data[:, :, 1], axis=1)
 
 
 
--- dataset_descriptor.py
+++ dataset_descriptor.py
@@ -3,13 +3,15 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-train_folder = "manual_sessions/dataset"
+train_folder = "manual_sessions/"
 plot_folder = "plots/"
+data_folder = "manual_sessions/"
 ignore_files = []
-to_exclude = ['ACC']#Exclude the Accelerometer data
-target_classes = ['mistake']#['classRelease' , 'classDepth', 'classRate', 'armsLocked', 'bodyWeight']
-tensor_data, annotations = data_helper.get_data_from_files(train_folder, ignore_files=ignore_files, res_rate=25,
-                                                           to_exclude=to_exclude)
+to_exclude = ['OenName', 'RecordingID', 'ApplicationName']
+target_classes = ['mistake_back']
+tensor_data, annotations, attributes = data_helper.get_data_from_files(data_folder, ignore_files=ignore_files,
+                                                                       res_rate=25,
+                                                                       to_exclude=to_exclude)
 print("Shape of the tensor_data is: " + str(np.shape(tensor_data)))
 print("Shape of the annotation is: " + str(np.shape(annotations)))
 
